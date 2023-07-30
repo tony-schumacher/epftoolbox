7c88c5a3e3aed24677eaa30efee997d13c419042
Tony Schumacher <schumacher.tony90@gmail.com>
2023-07-30 07:57:12 +0200

    plot benchmark

--- examples/recalibrating_lear_simplified.py
+++ examples/recalibrating_lear_simplified.py
@@ -11,25 +11,25 @@ import os
 
 # Market under study. If it not one of the standard ones, the file name
 # has to be provided, where the file has to be a csv file
-dataset = 'PJM'
+dataset = 'DE'
 
 # Number of years (a year is 364 days) in the test dataset.
-years_test = 2
+# years_test = 2
 
 # Number of days used in the training dataset for recalibration
-calibration_window = 364 * 4
+calibration_window = 300
 
 # Optional parameters for selecting the test dataset, if either of them is not provided, 
 # the test dataset is built using the years_test parameter. They should either be one of
 # the date formats existing in python or a string with the following format
 # "%d/%m/%Y %H:%M"
-begin_test_date = None
-end_test_date = None
+begin_test_date = "10/06/2023 00:00"
+end_test_date = "16/06/2023 00:00"
 
 path_datasets_folder = os.path.join('.', 'datasets')
 path_recalibration_folder = os.path.join('.', 'experimental_files')
     
 evaluate_lear_in_test_dataset(path_recalibration_folder=path_recalibration_folder, 
-                             path_datasets_folder=path_datasets_folder, dataset=dataset, years_test=years_test, 
+                             path_datasets_folder=path_datasets_folder, dataset=dataset, years_test=None, 
                              calibration_window=calibration_window, begin_test_date=begin_test_date, 
                              end_test_date=end_test_date)
